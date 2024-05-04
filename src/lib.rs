9b1f7bc54744e358cde8aba1881bf74b7fda65dc
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 12:39:42 +0200

    checking brackets

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!("Check if the string \"{string}\" contains balanced brackets");
+    let mut stack: Vec<char> = Vec::new();
+    let bracket_map: [(char, char); 3] = [(')', '('), ('}', '{'), (']', '[')];
+
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => stack.push(c),
+            ')' | '}' | ']' => {
+                if let Some(&top) = stack.last() {
+                    if let Some(&expected) = bracket_map.iter().find(|&&m| m.0 == c) {
+                        if top != expected.1 {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            _ => (),
+        }
+    }
+
+    stack.is_empty()
 }
